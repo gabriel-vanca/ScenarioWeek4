ba045b92d7b794c952270b928ef2312a49979012
2khc <kh.kelvinchan@gmail.com>
2017-02-21 21:27:57 +0000

    Start of the algoirthm

--- src/Graph.java
+++ src/Graph.java
@@ -1,6 +1,8 @@
+import DataStructures.Coordinates;
 import DataStructures.Node;
 import DataStructures.Robot;
 
+import java.awt.geom.Line2D;
 import java.util.ArrayList;
 
 /**
@@ -38,6 +40,35 @@ public class Graph{
                     node1.AddNeighbouringNode(node2);
             }
         }
+    }
+
+    public void findLineToRobots(DataStructures.Robot startNode) {
+        Map map = Map.GetInstance();
+
+
+        for (int i = 1; i < nodesList.size(); i++) {
+            Boolean isObstructed = false;
+            Coordinates startNodeCoordinates = startNode.GetCoordinates();
+            Coordinates targetNodeCoordinates = nodesList.get(i).GetCoordinates();
+
+            Line2D line = new Line2D.Double(startNodeCoordinates.x, startNodeCoordinates.y,
+                    targetNodeCoordinates.x, targetNodeCoordinates.y);
+
+            for (int j = 0; j < map.obstaclesList.size(); j++) {
+
+                // If line intersects line, find away around obstacle.
+                if (line.intersectsLine(map.obstaclesList.get(j))) {
+                    isObstructed = true;
+                }
+            }
+
+            // If line to robot is unobstructed, then create an edge between the two robots.
+
+
+
+        }
+
+
     }
 
 }
