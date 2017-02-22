ca7da95b52f0fc5399de77e9d99c4280031a7ed4
2khc <kh.kelvinchan@gmail.com>
2017-02-22 00:59:00 +0000

    Crafting the algorithm.

--- src/Graph.java
+++ src/Graph.java
@@ -28,27 +28,86 @@ public class Graph {
     }
 
 
-    public void findLineToRobots(DataStructures.Robot startNode) {
+    public void findLineToRobots(Node startNode, Node targetNode) {
         Map map = Map.GetInstance();
 
-
+        // Iterate through the list of nodes, drawing a straight line to every Node that we can,
+        // then for the ones we can't find a way around.
         for (int i = 1; i < nodesList.size(); i++) {
+
             Boolean isObstructed = false;
+
+            // Get the startNode coords, and the targetNode coords, then create a Line2D between these two points.
             Coordinates startNodeCoordinates = startNode.GetCoordinates();
             Coordinates targetNodeCoordinates = nodesList.get(i).GetCoordinates();
 
             Line2D line = new Line2D.Double(startNodeCoordinates.x, startNodeCoordinates.y,
                     targetNodeCoordinates.x, targetNodeCoordinates.y);
 
+            // Check the Line2D against all obstacles.
+            int tempX;
+            int tempY;
+            double distToLine = Double.MAX_VALUE;
+            ArrayList<Line2D> closestLines = new ArrayList<>();
+
             for (int j = 0; j < map.obstaclesList.size(); j++) {
 
-                // If line intersects line, find away around obstacle.
-                if (line.intersectsLine(map.obstaclesList.get(j))) {
+                Line2D obstacle = map.obstaclesList.get(j);
+
+                // If line intersects line in obstacleList, find away around obstacle.
+                if (line.intersectsLine(obstacle)) {
+
+
+                    double distFromStartNode = obstacle.ptLineDist(startNodeCoordinates.x, startNodeCoordinates.y);
                     isObstructed = true;
+
+                    // At most, a corner of a Line2D will coincide with the corner of another Line2D. We need
+                    // to find the closest two Line2Ds.
+
+                    if (distFromStartNode < distToLine) {
+
+                        // Update the closest line distance, then replace the stored Line2D with the new Line2D.
+
+                        distToLine = distFromStartNode;
+
+                        // If no Line2D stored yet, simply add the Line2D.
+                        if (closestLines.size() == 0) {
+                            closestLines.add(obstacle);
+                        } else {
+
+                            // Clear old lines, add new Line.
+                            closestLines.clear();
+                            closestLines.add(obstacle);
+                        }
+
+                    } else if (distFromStartNode == distToLine) {
+
+                        // In this case, we have found another Line2D which is the same distance from an
+                        // already stored Line2D.
+
+                        closestLines.add(obstacle);
+
+
+                    }
+
                 }
             }
 
             // If line to robot is unobstructed, then create an edge between the two robots.
+            if (!isObstructed) {
+                // Add node to list.
+                AddNode(new Node(targetNodeCoordinates.x, targetNodeCoordinates.y));
+            } else {
+                // Now we need to continue finding a path around the obstacle.
+                // We will recurse starting from both ends of a line.
+
+                // Create Node at both ends of line.
+                Node node1 = new Node(closestLines.get(0).getX1(), closestLines.get(0).getY1());
+                Node node2 = new Node(closestLines.get(0).getX2(), closestLines.get(0).getY2());
+
+                findLineToRobots(node1, targetNode);
+                findLineToRobots(node2, targetNode);
+            }
 
 
         }
