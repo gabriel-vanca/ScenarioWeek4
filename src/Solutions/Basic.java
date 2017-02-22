6c9dbc126cf2a1782c209656ab1d2e48207674ca
azinonos <zin_onos@hotmail.com>
2017-02-22 19:44:01 +0000

    Built Basic Class

--- src/Solutions/Basic.java
+++ src/Solutions/Basic.java
@@ -1,16 +1,40 @@
 package Solutions;
 
+import DataStructures.Node;
 import com.frappe.main.Graph;
+import com.frappe.main.Pathfinder;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 /**
- * Created by kelv on 22/02/17.
+ * Created by bad guy kelv on 22/02/17.
  */
 public class Basic {
-    Graph graph;
+
+    public Basic() {
+        Graph graph = Graph.GetInstance();
+        ArrayList<Node> nodesWithRobots = new ArrayList<>();
+        Pathfinder pathfinder = new Pathfinder();
+        StringBuilder sb = new StringBuilder();
+
+        ArrayList<Node> solutionNodes = new ArrayList<>();
+
+        for (Node n: graph.nodesList) {
+            if(!n.robotsAtThisNode.isEmpty()) {
+                nodesWithRobots.add(n);
+            }
+        }
+
+        for (int i = 0; i < nodesWithRobots.size()-1; i++) {
+            ArrayList<Node> temp = pathfinder.findShortestPathAStar(nodesWithRobots.get(i),nodesWithRobots.get(i+1));
+            //Collections.reverse(temp);
+                solutionNodes.addAll(temp);
+        }
 
 
-    public Basic(Graph graph) {
-        this.graph = graph;
+
     }
-
 }
