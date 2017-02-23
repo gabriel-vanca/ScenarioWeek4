6247200161cbfed75d500e67661a24a40fddafef
Lambros Zannettos <zcablza@ucl.ac.uk>
2017-02-23 20:50:26 +0000

    final

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,7 +1,13 @@
-package PACKAGE_NAME;
+import java.awt.geom.Line2D;
+import java.util.LinkedList;
 
 /**
  * Created by Lambros on 20/02/2017.
  */
 public class Obstacle {
+
+    LinkedList<Line2D> edges;
+
+
+
 }
